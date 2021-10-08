6a6755d341f49088c933a13d63397ee007774bee
Hannu Hanhi <hhanhipublic@gmail.com>
2021-10-08 22:47:22 +0300

    Request 24-bit depth buffer from SDL
    Should fix problems caused by 16-bit depth buffer usage

--- src/sdl/i_video.c
+++ src/sdl/i_video.c
@@ -1783,6 +1783,11 @@ static SDL_bool Impl_CreateWindow(SDL_bool fullscreen)
 #ifdef HWRENDER
 	if (rendermode == render_opengl)
 		flags |= SDL_WINDOW_OPENGL;
+
+	// Without a 24-bit depth buffer many visuals are ruined by z-fighting.
+	// Some GPU drivers may give us a 16-bit depth buffer since the
+	// default value for SDL_GL_DEPTH_SIZE is 16.
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 #endif
 
 	// Create a window
